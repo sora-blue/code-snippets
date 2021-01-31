7cb8f8d259ad6a09bbe9c16c3090ea0e997f9b05
BlueStar233 <56759083+BlueStar233@users.noreply.github.com>
2021-01-30 18:58:52 -0800

    Create windows-h-mouse-simulator.c

---
+++ windows-h-mouse-simulator.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+//SetCursorPos(x, y);
+//mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+//MessageBox(0,"1","2",0);
+int main(){
+	return 0;
+}
