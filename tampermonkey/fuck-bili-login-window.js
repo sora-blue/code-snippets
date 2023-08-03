1fda88accaac51a06ca1cd26d92d6238eadbe950
sora-blue <56759083+sora-blue@users.noreply.github.com>
2023-08-03 18:27:34 +0800

    Update fuck-bili-login-window.js

--- tampermonkey/fuck-bili-login-window.js
+++ tampermonkey/fuck-bili-login-window.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         FuckBiliLoginWindow
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.2
+// @description  Also fuck Google in the future for promoting DRM-ed Web
 // @author       You
 // @match        https://www.bilibili.com/video/*
+// @match        https://search.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==
@@ -20,14 +21,17 @@
 
     window.addEventListener('load', () => {
         setInterval(() => {
+            let targets = ['.login-tip', '.bili-mini-mask', '.bpx-player-toast-wrap']
+            for(let i = 0 ; i < targets.length ; i++){
+                let a = document.querySelector(targets[i]);
+                if(!a) continue;
+                a.remove();
+            }
+
             let c = document.querySelector('.bpx-player-state-play')
-            if(window.getComputedStyle(c)['display'] != 'none'){
+            if(c && window.getComputedStyle(c)['display'] != 'none'){
                 sendPlay()
             }
-            let a = document.querySelector('.bili-mini-mask');
-            if(!a) return;
-            a.remove();
-            sendPlay()
         }, 300)
     })
 })();
